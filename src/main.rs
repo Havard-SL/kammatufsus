aa1fa5df305062f27391aa647c09dd4d17576d17
Havard-SL <Hovardslill@gmail.com>
2025-08-03 14:53:09 +0200

    Fekk lyd til å fungera halvvegs, lagde favicon.

--- src/main.rs
+++ src/main.rs
@@ -4,10 +4,7 @@ use std::{
     net::{TcpListener, TcpStream},
 };
 
-enum FileType {
-    html,
-    mp3,
-}
+const VALID_FILES: [&str; 3] = ["hello.html", "pumpupjam.mp3", "favicon.ico"];
 
 fn main() {
     println!("Hello, world!");
@@ -26,27 +23,31 @@ fn handle_connection(mut stream: TcpStream) {
     let buf_reader = BufReader::new(&mut stream);
     let request_line = buf_reader.lines().next().unwrap().unwrap();
 
-    println!("{}", &request_line);
+    let mut request_line = request_line.split(' ');
 
-    let (status_line, filename, filetype) = match &request_line[..] {
-        "GET / HTTP/1.1" => ("HTTP/1.1 200 OK", "hello.html", FileType::html),
-        "GET /pumpupjam.mp3 HTTP/1.1" => ("HTTP/1.1 200 OK", "pumpupjam.mp3", FileType::mp3),
-        _ => ("HTTP/1.1 404 NOT FOUND", "404.html", FileType::html),
-    };
+    let command = request_line.next().unwrap();
+    let mut filename = &request_line.next().unwrap()[1..];
+    let http_version = request_line.next().unwrap();
 
-    let response =  match filetype {
-        FileType::html => {
-            let contents = fs::read_to_string(filename).unwrap();
-            let message = format!("{status_line}\r\nContent-Length: {}\r\n\r\n{contents}", contents.len());
-            message.as_bytes()
-        },
-        FileType::mp3 => {
-            todo!();
-            let contents = fs::read(filename).unwrap();
-            let message = format!("{status_line}\r\nContent-Length: {}\r\n\r\n", contents.len()).as_bytes();
-            &vec![message, &contents[..]].concat()
-        },
-    };
+    let status_line;
+
+    if filename.is_empty() {
+        filename = "hello.html";
+    }
+
+    println!("{}", filename);
+
+    if command == "GET" && VALID_FILES.contains(&filename) && http_version == "HTTP/1.1" {
+        status_line = "HTTP/1.1 200 OK";
+    } else {
+        status_line = "HTTP/1.1 404 NOT FOUND";
+        filename = "404.html"
+    }
+
+    let contents = fs::read(filename).unwrap();
+    
+    let message = format!("{status_line}\r\nContent-Length: {}\r\n\r\n", contents.len());
+    let response =  &[message.as_bytes(), &contents[..]].concat();
 
     stream.write_all(response).unwrap();
     // println!("Request: {http_request:#?}");
